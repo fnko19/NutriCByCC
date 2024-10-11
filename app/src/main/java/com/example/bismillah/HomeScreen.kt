b3c1500b938bb56e06ba1479b7adb0f2a1e0df7d
fnko19 <nchuuya76@gmail.com>
2024-10-11 13:44:59 +0700

    initial commit

--- app/src/main/java/com/example/bismillah/HomeScreen.kt
+++ app/src/main/java/com/example/bismillah/HomeScreen.kt
@@ -1,9 +1,7 @@
 package com.example.bismillah
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
