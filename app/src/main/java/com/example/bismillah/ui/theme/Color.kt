22b9f007c2726507c2d4d1d2a7abdc4ec20d34b1
fnko19 <nchuuya76@gmail.com>
2024-10-11 13:26:05 +0700

    coba

---
+++ app/src/main/java/com/example/bismillah/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.bismillah.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val YaleBlue = Color(0xFF00356B)
+val LemonChiffon = Color(0xFFFAF0CA)
+val NaplesYellow = Color(0xFFF4D35E)
+val SandyBrown = Color(0xFFEE964B)
+val Tomato = Color(0xFFF95738)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
